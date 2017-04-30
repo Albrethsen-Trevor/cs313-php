1467f0a7aa8c71aaa2f1e11dad8375afba3f6aa5
Albrethsen-Trevor <alb12014@byui.edu>
2017-04-29 20:24:34 -0600

    added index.php, assignments.php, and main.css

---
+++ web/assignments.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome Home</title>
+        <link rel="stylesheet" href="/main.css">
+    </head>
+    <body>
+        <header>
+            <h1>CS 313: Web Engineering II</h1>
+            <nav>  
+                <div class="topnav" id="myTopnav">
+                  <a href="/index.php">Home</a>
+                  <a class="active" href="/assignments.php">Assignments</a>
+                </div>
+            </nav>
+        </header>
+        
+        <main>
+            <section>
+                <div>
+                    <h2>Under Construction...</h2>
+                    <p>More coming soon...</p>
+                </div>
+            </section>
+            
+        </main>
+    </body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,31 @@
-<?php
-
-  phpinfo();
-
-?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome Home</title>
+        <link rel="stylesheet" href="/main.css">
+    </head>
+    <body>
+        <header>
+            <h1>CS 313: Web Engineering II</h1>
+            <nav>  
+                <div class="topnav">
+                  <a class="active" href= "/index.php">Home</a>
+                  <a href="/assignments.php">Assignments</a>
+                </div>
+            </nav>
+        </header>
+        
+        <main>
+            <section>
+                <div>
+                    <h2>About Me</h2>
+                    <p class="boxed">My name is Trevor Albrethsen. I am developing a web page for CS 313 using HTML, CSS, and PHP. Hopefully this looks better soon.</p>
+                    <p class="boxed">While I haven't done much hiking in Idaho, I enjoyed going on hikes in Oregon. I'm also an Oregon Ducks fan as you may have noticed by the shirt I'm wearing in the photo below.</p>
+                    <center><img src=https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/260344_2035701765751_3782425_n.jpg?oh=9f72e8e7e9b58e2d86b712002a98289e&oe=59C2F8B1></center>
+                </div>
+            </section>
+            
+        </main>
+    </body>
+</html>
