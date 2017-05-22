b5559aed04035974b8136948dac13f8684615d8d
Albrethsen-Trevor <alb12014@byui.edu>
2017-05-22 15:25:33 -0600

    added php file and connected to database hopefully

--- web/citadelstores.php
+++ web/citadelstores.php
@@ -21,6 +21,24 @@ catch (PDOException $ex)
 	die();
 }
 
+<?php
+$dbconn = pg_connect('dbname=dcd7qqpqqovmfr');
+//connect to a database named "mary"
+
+$dbconn2 = pg_connect('host=ec2-23-23-227-188.compute-1.amazonaws.com port=5432 dbname=dcd7qqpqqovmfr');
+// connect to a database named "mary" on "localhost" at port "5432"
+
+$dbconn3 = pg_connect("host=ec2-23-23-227-188.compute-1.amazonaws.com port=5432 dbname=dcd7qqpqqovmfr user=rvhisotzxirgct password=1d5f38daad8d3164a83ea5d9f27a0d5efe65eba86056abcf948b360ccf31b4bf");
+//connect to a database named "mary" on the host "sheep" with a username and password
+
+$conn_string = 'host=ec2-23-23-227-188.compute-1.amazonaws.com port=5432 dbname=dcd7qqpqqovmfr user=rvhisotzxirgct password=1d5f38daad8d3164a83ea5d9f27a0d5efe65eba86056abcf948b360ccf31b4bf';
+$dbconn4 = pg_connect($conn_string);
+//connect to a database named "test" on the host "sheep" with a username and password
+
+$dbconn5 = pg_connect("host=ec2-23-23-227-188.compute-1.amazonaws.com options='--client_encoding=UTF8'");
+//connect to a database on "localhost" and set the command line parameter which tells the encoding is in UTF-8
+?>
+
 ?>
 
 <!DOCTYPE html>
