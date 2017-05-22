1353d844a38a2a333480c4546655f305cda56449
Albrethsen-Trevor <alb12014@byui.edu>
2017-05-22 15:34:01 -0600

    added php file and connected to database hopefully

--- web/citadelstores.php
+++ web/citadelstores.php
@@ -1,29 +1,4 @@
 <?php
-
-$dbUser = 'rvhisotzxirgct';
-$dbPassword = '1d5f38daad8d3164a83ea5d9f27a0d5efe65eba86056abcf948b360ccf31b4bf';
-$dbName = 'dcd7qqpqqovmfr';
-$dbHost = 'ec2-23-23-227-188.compute-1.amazonaws.com';
-$dbPort '5432';
-
-try
-{
-	// Create the PDO connection
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex)
-{
-	// If this were in production, you would not want to echo
-	// the details of the exception.
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
-
-?>
-
-<?php
 $dbconn = pg_connect('dbname=dcd7qqpqqovmfr');
 //connect to a database named "mary"
 
