99670f7e67ff57a5826d88a317e3fd63e2d512d0
Albrethsen-Trevor <alb12014@byui.edu>
2017-05-30 01:03:43 -0600

    changes

--- web/citadelstores.php
+++ web/citadelstores.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Vote for Your Favorite Store in the Citadel</title>
+    <title>Stores Across the Universe</title>
 </head>
 <body>
-    <h1>List of Citadel Stores</h1>
+    <h1>List of Stores in the Universe</h1>
     
     <?php
 
 $dbconn = pg_connect("host=ec2-23-23-227-188.compute-1.amazonaws.com port=5432 dbname=dcd7qqpqqovmfr user=rvhisotzxirgct password=1d5f38daad8d3164a83ea5d9f27a0d5efe65eba86056abcf948b360ccf31b4bf")
 or die ("Try, and therefore sadness");
                 echo 'connected!';
-                $query = 'SELECT * FROM citadel_stores';
+                $query = 'SELECT * FROM store';
                 $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
                 echo 'queried!';
 ?>
