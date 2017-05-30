96cdbf090a6b773feca47127c5e2a7a7ac65e375
Albrethsen-Trevor <alb12014@byui.edu>
2017-05-30 11:58:15 -0600

    changes

--- web/citadelstores.php
+++ web/citadelstores.php
@@ -38,7 +38,7 @@ $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     
     <?php
         foreach ($results as $row) {
-            echo "<p>" . $row[''] . ' ' . $row['location'] . ' ' . $row['owner'] ."</p>";
+            echo "<p>" . $row['name'] . ' ' . $row['location'] . ' ' . $row['owner'] ."</p>";
             
             $counter++;
     }
