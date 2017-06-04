36f860ccdc9eb11615101173ce41739bc1eafdb9
Albrethsen-Trevor <alb12014@byui.edu>
2017-06-03 19:34:25 -0600

    new php and more changes

--- web/citadelstores.php
+++ web/citadelstores.php
@@ -4,12 +4,6 @@ require("dbConnect.php");
 $db = get_db();
 
 $query = 'SELECT title, location, owner FROM store';
-//$query = 'SELECT name, shipment FROM inventory';
-
-
-$query = 'SELECT title, location, owner FROM store';
-//$query = 'SELECT name, shipment FROM inventory';
-
 
 $statement = $db->prepare($query);
 $statement->execute();
@@ -22,10 +16,10 @@ $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 <!doctype html>
 <html>
 <head>
-    <title>Stores Across the Universe</title>
+    <title>Mass Effect Stores and User Ratings</title>
 </head>
 <body>
-    <h1>List of Stores</h1>
+    <h1>List of Stores and User Ratings</h1>
     
     <?php
         foreach ($results as $row) {
